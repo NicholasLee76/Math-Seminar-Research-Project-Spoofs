ee4e6c0360f6fdbce0d9c021fba5842bbdcfeb08
amymet <79421219+amymet@users.noreply.github.com>
2023-04-13 09:15:53 -0400

    Update quasi_almost_spoofs.cpp

--- quasi_almost_spoofs.cpp
+++ quasi_almost_spoofs.cpp
@@ -12,32 +12,40 @@ int sigma(int m) {
     return s;
 }
 
+bool check_quasi(int m, int s, int c) {
+    return s*(c*c+c+1) == 2*m*(c+1)+1 && c > 1;
+}
+
+bool check_almost(int m, int s, int c) {
+    return s*(c*c+c+1) == 2*m*(c+1)-1 && c > 1;
+}
+
 int main() {
     int limit = 10000000;
     for (int m=1; m<limit; m++) {
         if (m % 200000 == 0) {
-            cout << "Checking " << m << "th value" << endl;
+            cout << "Checking " << m << "... " << endl;
         }
         int s = sigma(m);
         int c_top = ceil(((2*m-s) + sqrt((s-2*m)*(s-2*m) - 4*s*(s-2*m+1)))/(2*s));
         int c_bot = floor(((2*m-s) + sqrt((s-2*m)*(s-2*m) - 4*s*(s-2*m+1)))/(2*s));
 
         // quasi
-        if (s*(c_top*c_top+c_top+1) == 2*m*(c_top+1)+1 && c_top > 1) {
+        if (check_quasi(m, s, c_top)) {
             cout << "c = " << c_top << ", m = " << m << ", quasi";
         }
 
-        if (s*(c_bot*c_bot+c_bot+1) == 2*m*(c_bot+1)+1 && c_bot > 1) {
+        if (check_quasi(m, s, c_bot)) {
             cout << "c = " << c_bot << ", m = " << m << ", quasi";
         }
 
         // almost
-        if (s*(c_top*c_top+c_top+1) == 2*m*(c_top+1)-1 && c_top > 1) {
+        if (check_almost(m, s, c_top)) {
             cout << "c = " << c_top << ", m = " << m << ", almost";
         }
 
-        if (s*(c_bot*c_bot+c_bot+1) == 2*m*(c_bot+1)-1 && c_bot > 1) {
-            cout << "c = " << c_top << ", m = " << m << ", almost";
+        if (check_almost(m, s, c_bot)) {
+            cout << "c = " << c_bot << ", m = " << m << ", almost";
         }
     }
 
